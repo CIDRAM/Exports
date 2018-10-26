076307dc5afbae7f640f4fdfbe65a676ca202f66
Caleb Mazalevskis <maikuolan@gmail.com>
2018-10-26 10:30:57 +0800

    Signatures update.

--- zb/ip4x_head.php
+++ zb/ip4x_head.php
@@ -14,9 +14,9 @@
 /**
  * Signature file version information.
  * See: http://zb-block.net/zbf/showthread.php?t=20
- * This file last updated: 2018.10.14
+ * This file last updated: 2018.10.26
  */
-$zbbversion .= ' / IP4X-85b';
+$zbbversion .= ' / IP4X-85c';
 
 // *php 5 fix
 global $whyblockout, $ax;
