a81bea39d62609ca1529a25afaf179bd546bc306
Caleb Mazalevskis <maikuolan@gmail.com>
2020-08-11 18:42:57 +0800

    Signatures update.

--- zb/ip4h_head.php
+++ zb/ip4h_head.php
@@ -14,9 +14,9 @@
 /**
  * Signature file version information.
  * See: http://zb-block.net/zbf/showthread.php?t=20
- * This file last updated: 2020.08.01
+ * This file last updated: 2020.08.11
  */
-$zbbversion .= ' / IP4H-106c';
+$zbbversion .= ' / IP4H-106d';
 
 // *php 5 fix
 global $whyblockout, $ax;
