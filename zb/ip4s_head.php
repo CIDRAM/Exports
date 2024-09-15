ad767a5e97b1fd6a21561bd75b23d4defa59c37e
Caleb Mazalevskis <maikuolan@gmail.com>
2024-09-15 16:49:49 +0800

    Signatures update.

--- zb/ip4s_head.php
+++ zb/ip4s_head.php
@@ -14,9 +14,9 @@
 /**
  * Signature file version information.
  * See: http://zb-block.net/zbf/showthread.php?t=20
- * This file last updated: 2024.09.01
+ * This file last updated: 2024.09.15
  */
-$zbbversion .= ' / IP4S-155';
+$zbbversion .= ' / IP4S-155a';
 
 // *php 5 fix
 global $whyblockout, $ax;
