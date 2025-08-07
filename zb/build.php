d579f0ad1dc4774465764bb75f40b8ab1d67396b
Caleb Mazalevskis <maikuolan@gmail.com>
2025-08-08 01:20:39 +0800

    Fix some typos.

--- zb/build.php
+++ zb/build.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * This file is a tool used for adapting the CIDRAM default IPv4 signature files to work with ZB Block 0.5+.
+ * This file is a tool used for adapting the CIDRAM default IPv4 signature files to work with ZB-Block 0.5+.
  *
  * Homepage for CIDRAM: https://cidram.github.io/
  * CIDRAM COPYRIGHT 2016 and beyond by Caleb Mazalevskis (Maikuolan).
  *
- * Homepage for ZB Block 0.4.20 and above: http://zb-block.net
+ * Homepage for ZB-Block 0.4.20 and above: http://zb-block.net
  *
  * License: GNU/GPLv2
  * @see LICENSE.txt
